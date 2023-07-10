af1339f888421f95504b319560bed5932d949d0c
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-07-10 11:23:33 +0300

    const instead of var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var number1 = parseFloat(prompt("Введіть перше число:"));
-var number2 = parseFloat(prompt("Введіть друге число:"));
-var number3 = parseFloat(prompt("Введіть третє число:"));
+const number1 = parseFloat(prompt("Введіть перше число:"));
+const number2 = parseFloat(prompt("Введіть друге число:"));
+const number3 = parseFloat(prompt("Введіть третє число:"));
 
-var average = (number1 + number2 + number3) / 3;
+const average = (number1 + number2 + number3) / 3;
 
 alert("Середнє арифметичне: " + average);
